85e716c2cbe534f8b30f6fc93e86824c617641a4
Matheus Goveia <mateuus.goveia@gmail.com>
2025-11-03 11:29:21 -0300

    Criamos objetos das classes
    demonstrando como a herança pode ser utilizada para compartilhar funcionalidades entre diferentes tipos de itens de cardápio.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'gourmet')
-restaurante_praca.receber_avaliacao('Gui', 5)
-restaurante_praca.receber_avaliacao('Lais', 4)
-restaurante_praca.receber_avaliacao('Emy', 1)
+bebida_suco = Bebida('Suco de melância', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
+    
